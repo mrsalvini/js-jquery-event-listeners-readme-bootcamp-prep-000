73f44755d101abc585ff2c8d33503d47cb62f26c
Diogo Costa <dsgmcosta@gmail.com>
2019-02-05 13:12:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  alert("hey!");
+}
+
+function frameIt() {
+  $("img").addClass("tasty")
+}
+
+function pressIt() {
+  $("#typing").on('keydown', function(key) {
+    if(key.which == 71) {
+      alert('G was pressed');
+    }
+  })
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+}
+
+
+
+$(document).ready(function() {
 
 // call functions here
+
+$("p").on("click", getIt)
+
+frameIt()
+
+pressIt()
+
+$("form").on("submit", submitIt)
 
 });
