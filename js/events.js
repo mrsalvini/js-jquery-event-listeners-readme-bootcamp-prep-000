b6e9f58309d09e4066e10f615d3bd04efa6ac337
Diogo Costa <dsgmcosta@gmail.com>
2019-02-05 15:03:29 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -8,12 +8,20 @@ function frameIt() {
   $("img").addClass("tasty")
 }
 
+<<<<<<< HEAD
 function pressIt() {
   $("#typing").on('keydown', function(key) {
     if(key.which == 71) {
       alert('G was pressed');
     }
   })
+=======
+function pressIt(event) {
+  var pressed = event.key
+  if (pressed == "G") {
+      alert('G was pressed');
+  }
+>>>>>>> dab68e3b634da2e4302860bedff7b8a4766628fe
 }
 
 function submitIt() {
@@ -30,7 +38,11 @@ $("p").on("click", getIt)
 
 frameIt()
 
+<<<<<<< HEAD
 pressIt()
+=======
+$("#typing").on("keydown", pressIt)
+>>>>>>> dab68e3b634da2e4302860bedff7b8a4766628fe
 
 $("form").on("submit", submitIt)
 
